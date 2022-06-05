7b461104ce4447fc09bd39d14bcca06246dac65e
Remiel0306 <104558225+Remiel0306@users.noreply.github.com>
2022-06-05 17:01:45 +0800

    更改名字系統

--- Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/DataManager.cs
+++ Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/DataManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using ExitGames.Client.Photon.StructWrapping;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -10,17 +11,21 @@ namespace remiel
     public class DataManager : MonoBehaviour
     {
         // 新增部屬做的網址，有變更gas都要更新
-        private string gasLink = "https://script.google.com/macros/s/AKfycbzazl0klhkxSAlmVBuU5Z5drZdBBNynWCS9DqrOgZ1FGeUDzivCg-uH7v1Npyg3HYUeUw/exec";
+        private string gasLink =  "https://script.google.com/macros/s/AKfycbzoduwYKFNQwqWjiaeIja7WEmH9D8it95IgW9SSnIwyYvzpxEmh2bwB2-BL8SLGVwBi_A/exec";
         private WWWForm form;
 
         private Button btnGetData;
         private Text textPlayerName;
+        private TMP_InputField inputField;
 
         private void Start()
         {
             btnGetData = GameObject.Find("取得玩家資料按鈕").GetComponent<Button>();
             textPlayerName = GameObject.Find("玩家名稱").GetComponent<Text>();
             btnGetData.onClick.AddListener(GetGASData);
+
+            inputField = GameObject.Find("更新玩家名稱").GetComponent<TMP_InputField>();
+            inputField.onEndEdit.AddListener(SetGASData);
         }
 
         /// <summary>
@@ -43,6 +48,25 @@ namespace remiel
                 yield return www.SendWebRequest();
                 // 玩家名稱 = 連線要求下載的文字訊息
                 textPlayerName.text = www.downloadHandler.text;
+            }
+        }
+
+        private void SetGASData(string value)
+        {
+            form = new WWWForm();
+            form.AddField("method", "設定");
+            form.AddField("plauerName", inputField.text);
+
+            StartCoroutine(StartSetGASData());
+        }
+
+        private IEnumerator StartSetGASData()
+        {
+            using (UnityWebRequest www = UnityWebRequest.Post(gasLink, form))
+            {
+                yield return www.SendWebRequest();
+                textPlayerName.text = inputField.text;
+                print(www.downloadHandler.text);
             }
         }
     }
